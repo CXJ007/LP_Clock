36240b6ab88a70c8b5b38f1cb17c9242a6514ddd
CXJ007 <a592053746@163.com>
2024-09-08 18:13:56 +0800

    调整文件结构

---
+++ Software_RT/bsp/STM32L432xx/packages/bh1750fvi/inc/bh1750fvi.h
@@ -0,0 +1,37 @@
+#ifndef __BH1750FVI_H__
+#define __BH1750FVI_H__
+/*******************************************************************************
+***********************************Includes*************************************
+*******************************************************************************/
+#include "bh1750fvi_cfg.h"
+#include "drv_common.h"
+/*******************************************************************************
+*******************************Macro Definitions********************************
+*******************************************************************************/
+#define BH1750FVI_POWER_DOWN_CMD                               (0x00U)
+#define BH1750FVI_POWER_ON_CMD                                 (0x01U)
+#define BH1750FVI_SOFT_RESET_CMD                               (0x07U)
+#define BH1750FVI_CONTIN_1P0_CMD                               (0x10U)
+#define BH1750FVI_CONTIN_0P5_CMD                               (0x11U)
+#define BH1750FVI_CONTIN_4P0_CMD                               (0x13U)
+#define BH1750FVI_ONCE_1P0_CMD                                 (0x20U)
+#define BH1750FVI_ONCE_0P5_CMD                                 (0x21U)
+#define BH1750FVI_ONCE_4P0_CMD                                 (0x23U)
+
+#define BH1750FVI_SENSOR_DATA_BYTE_LEN                         (2U)
+
+#define BH1750FVI_ACCURACY                                     (1.2F)
+
+#ifdef  BH1750FVI_SENSOR_NAME
+#define BH1750FVI_SENSOR_RANGE_MIN                             (0U)
+#define BH1750FVI_SENSOR_RANGE_MAX                             (54612U)
+#define BH1750FVI_SENSOR_PERIOD_MS_MIN                         (0U)  
+#define BH1750FVI_SENSOR_FIFO_MAX                              (5U)  
+#endif
+
+/*******************************************************************************
+****************************Global Function Definitions*************************
+*******************************************************************************/
+extern int bh1750fvi_sensor_init(void);
+
+#endif
